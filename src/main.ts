6f08ce49dac19cde4f3e25937247dd166836bb98
sergiuchilat <chilatsergiu@gmail.com>
2022-09-18 01:56:02 +0300

    Add request validation

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,16 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 import buildApiDocs from './docs/swagger.builder';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   buildApiDocs(app);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      enableDebugMessages: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
--- src/modules/country/dto/country.create.dto.ts
+++ src/modules/country/dto/country.create.dto.ts
@@ -1,9 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Length } from 'class-validator';
 
 export class CountryCreateDto {
   @ApiProperty({ example: 'Name', description: 'Country name' })
+  @Length(3, 50, {
+    message: 'Name must contain [3, 50] characters',
+  })
   name: string;
 
   @ApiProperty({ example: 'AA', description: 'Country code' })
+  @Length(2, 2, {
+    message: 'Code must contain 2 characters',
+  })
   code: string;
 }

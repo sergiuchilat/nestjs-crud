85fae7b1ef6cc4a1e0a3fb9786d51aadd878d30e
sergiuchilat <chilatsergiu@gmail.com>
2022-09-18 02:04:24 +0300

    Add request validation(dynamic messages)

--- src/modules/country/dto/country.create.dto.ts
+++ src/modules/country/dto/country.create.dto.ts
@@ -4,13 +4,13 @@ import { Length } from 'class-validator';
 export class CountryCreateDto {
   @ApiProperty({ example: 'Name', description: 'Country name' })
   @Length(3, 50, {
-    message: 'Name must contain [3, 50] characters',
+    message: 'Name must contain [$constraint1, $constraint2] characters',
   })
   name: string;
 
   @ApiProperty({ example: 'AA', description: 'Country code' })
   @Length(2, 2, {
-    message: 'Code must contain 2 characters',
+    message: 'Code must contain $constraint1 characters',
   })
   code: string;
 }

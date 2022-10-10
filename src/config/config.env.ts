dcc240f236387194273bcb837923cd61b3081c39
sergiuchilat <chilatsergiu@gmail.com>
2022-10-10 10:24:23 +0300

    Replace nest-dotenv with dotenv

--- src/config/config.env.ts
+++ src/config/config.env.ts
@@ -1,28 +1,24 @@
-import { ConfigService } from 'nestjs-dotenv';
-
-const configService: ConfigService = new ConfigService();
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const ConfigEnv = {
-  APP_DEV_MODE: configService.get('APP_DEV_MODE'),
-  API_SERVER_URL: configService.get('API_SERVER_URL'),
-  API_SERVER_PORT: configService.get('API_SERVER_PORT'),
-  DB_HOST: configService.get('DB_HOST'),
-  DB_PORT: configService.get('DB_PORT'),
-  DB_USER: configService.get('DB_USER'),
-  DB_PASSWORD: configService.get('DB_PASSWORD'),
-  DB_NAME: configService.get('DB_NAME'),
-  DB_DRIVER: configService.get('DB_DRIVER'),
-  JWT_BEARER_AUTH_NAME: configService.get('JWT_BEARER_AUTH_NAME'),
-  JWT_TOKEN_EXPIRES_IN: configService.get('JWT_TOKEN_EXPIRES_IN'),
-  JWT_SECRET_KEY: configService.get('JWT_SECRET_KEY'),
-  DOCS_GENERATE: configService.get('DOCS_GENERATE'),
-  DOCS_PATH: configService.get('DOCS_PATH'),
-  DOCS_VERSION: configService.get('DOCS_VERSION'),
-  DOCS_TITLE: configService.get('DOCS_TITLE'),
-  DOCS_DESCRIPTION: configService.get('DOCS_DESCRIPTION'),
-  APP_DEFAULT_TIMEOUT: configService.getWithType(
-    'APP_DEFAULT_TIMEOUT',
-    'number',
-  ),
+  APP_DEV_MODE: process.env.APP_DEV_MODE,
+  API_SERVER_URL: process.env.API_SERVER_URL,
+  API_SERVER_PORT: process.env.API_SERVER_PORT,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME,
+  DB_DRIVER: process.env.DB_DRIVER,
+  JWT_BEARER_AUTH_NAME: process.env.JWT_BEARER_AUTH_NAME,
+  JWT_TOKEN_EXPIRES_IN: process.env.JWT_TOKEN_EXPIRES_IN,
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+  DOCS_GENERATE: process.env.DOCS_GENERATE,
+  DOCS_PATH: process.env.DOCS_PATH,
+  DOCS_VERSION: process.env.DOCS_VERSION,
+  DOCS_TITLE: process.env.DOCS_TITLE,
+  DOCS_DESCRIPTION: process.env.DOCS_DESCRIPTION,
+  APP_DEFAULT_TIMEOUT: Number(process.env.APP_DEFAULT_TIMEOUT),
 };
 export default ConfigEnv;
